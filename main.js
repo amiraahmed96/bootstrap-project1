a72b7af2d01e6f0068b413daaf6694589699ad4b
Amira Ahmed <amira.ahmed@code59.info>
2018-11-29 18:27:04 +0200

    bootstrap project

---
+++ main.js
@@ -0,0 +1,8 @@
+var clic = document.getElementById("clic"),
+    list = document.getElementById("list");
+clic.onclick = function () {
+    list.classList.toggle("display");
+    list.classList.toggle("display1");
+    };
+
+
